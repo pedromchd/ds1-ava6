d7b2b4f8dda29d91546cedf798a844b7255c0792
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-10-15 12:58:33 +0000

    dark mode setting

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import cx from 'classnames';
+
+function App() {
+    return (
+        <div className="dark">
+            <h1 className="text-3xl font-bold underline dark:text-red-600 text-black">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
+
+export default App;
