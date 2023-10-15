3b9abcd696e4c7c980b9470c94f1c6fc3b9cd2c7
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-10-15 12:14:26 +0000

    tailwindcss install

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import cx from 'classnames';
 import './index.css';
-import App from './App';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
