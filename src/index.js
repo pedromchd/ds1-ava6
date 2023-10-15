72d82083ed11b94ae70cf373ee8bcf5799f5810b
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-10-15 12:39:47 +0000

    react app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import cx from 'classnames';
 import './index.css';
-
-function App() {
-    return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
-}
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
