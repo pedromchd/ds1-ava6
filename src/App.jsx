1f7d77ece86228092c8063d693258fe409345dcc
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-10-15 20:52:42 +0000

    dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState, useEffect } from 'react';
 import cx from 'classnames';
 
 function App() {
+    const [darkMode, setDarkMode] = useState(false);
+
     return (
-        <div className="dark">
-            <h1 className="text-3xl font-bold underline dark:text-red-600 text-black">
-                Hello world!
-            </h1>
+        <div className={cx('font-["Open_Sans"]', { 'dark': darkMode })}>
+            {/* <button onClick={() => setDarkMode(!darkMode)}>Set dark mode</button> */}
+
         </div>
     )
 }
